e0d7515af6dd446a6b7d0686d7ea1d331830b30c
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-08-28 18:12:54 +0300

    Update main.c

--- Solve-C/12-students-marks-sum/main.c
+++ Solve-C/12-students-marks-sum/main.c
@@ -8,8 +8,8 @@
 int marks_summation(int* marks, int number_of_students, char gender) {
   //Write your code here.
     int sum = 0;
-    for (int i = (gender == 'b' ? 0 : 1); i < number_of_students; i = i + 2) //if gender equal to b i =  0 otherwise (gender = g) i = 1 
-        sum += *(marks + i);
+    for (int i=(gender == 'b' ? 0 : 1); i<number_of_students; i=i+2) //if gender equal to b i =  0 otherwise (gender = g) i = 1. ? is a conditional operator.
+        sum += *(marks+i);
     
     return sum;
 }
