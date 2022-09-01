b864a233d5192621cf5e00ec61163653d3b702fa
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-09-01 22:06:29 +0300

    Update main.c

--- Solve-C/17-digit-frequency/main.c
+++ Solve-C/17-digit-frequency/main.c
@@ -9,7 +9,7 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     char string; 
     int arr[10] = {0,0,0,0,0,0,0,0,0,0};
-    while (scanf("%c",&string) != EOF) 
+    while (scanf("%c",&string) != EOF) //break the loop when string's characters' done. End Of File
     {
         if(string >= '0' && string <= '9')
             arr[string-'0'] +=1; //increment by one to that element of string . for more information cc4e.com chapter 1 arrays
