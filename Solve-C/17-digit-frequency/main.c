9643c0f0724f15edb2d82ad794ca955ea767d376
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-09-01 22:04:30 +0300

    Create main.c

---
+++ Solve-C/17-digit-frequency/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    char string; 
+    int arr[10] = {0,0,0,0,0,0,0,0,0,0};
+    while (scanf("%c",&string) != EOF) 
+    {
+        if(string >= '0' && string <= '9')
+            arr[string-'0'] +=1; //increment by one to that element of string
+    }
+    
+    for(int i; i<10;i++)
+        printf("%d ",arr[i]);
+    return 0;
+}
