e39d18d84fbb8a09a6b8b72ab735fe5c2975e800
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-09-02 13:20:27 +0300

    Update main.cpp

--- C++/03-basic-data-types/main.cpp
+++ C++/03-basic-data-types/main.cpp
@@ -10,8 +10,9 @@ int main() {
     float f;
     double d;
     
-    cin >> i >> l >> c >> f >> d;
-    cout << i << endl << l << endl << c << endl << f << endl << d << endl;
+    scanf("%d %ld %c %f %lf",&i, &l, &c ,&f ,&d);
+    printf("%d\n%ld\n%c\n%f\n%lf\n",i,l,c,f,d);
+    
     
     return 0;
 }
