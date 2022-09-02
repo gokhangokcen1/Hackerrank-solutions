aafe7e10991e97af62624d0dc3faab48f6e4e102
Gökhan <63648396+gokhangokcen1@users.noreply.github.com>
2022-09-02 13:18:38 +0300

    Create main.cpp

---
+++ C++/03-basic-data-types/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    
+    cin >> i >> l >> c >> f >> d;
+    cout << i << endl << l << endl << c << endl << f << endl << d << endl;
+    
+    return 0;
+}
